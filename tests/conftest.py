5f48b94abc8142aeb32c4a74919d132aa64bf232
Steve Yang <steve.yang@uber.com>
2020-01-09 12:27:17 -0800

    multiple python versions and update test resources

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,11 +1,16 @@
 import pytest
 import pandas as pd
+import pkg_resources
 
 
 @pytest.fixture
 def iclaims_training_data():
+    test_file = pkg_resources.resource_filename(
+        'tests',
+        'resources/iclaims.example.csv'
+    )
     df = pd.read_csv(
-        'tests/resources/iclaims.example.csv',
+        test_file,
         parse_dates=['week']
     )
     return df
