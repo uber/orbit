d37320a122a3e31a17dc366319660f8ed7d115e5
Steve Yang <steve.yang@uber.com>
2020-01-09 17:41:50 -0800

    debug cython dist bug

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from __future__ import print_function
 import sys
 
 from setuptools import setup, find_packages
-# from setuptools.command.test import test as TestCommand
+from setuptools.command.test import test as TestCommand
 
 VERSION = '0.4.0'
 DESCRIPTION = "Orbit is a package for bayesian time series modeling and inference."
@@ -20,16 +20,16 @@ def requirements(filename="requirements.txt"):
         return f.readlines()
 
 
-# class PyTest(TestCommand):
-#     def finalize_options(self):
-#         TestCommand.finalize_options(self)
-#         self.test_args = []
-#         self.test_suite = True
-#
-#     def run_tests(self):
-#         import pytest
-#         errcode = pytest.main(self.test_args)
-#         sys.exit(errcode)
+class PyTest(TestCommand):
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
+
+    def run_tests(self):
+        import pytest
+        errcode = pytest.main(self.test_args)
+        sys.exit(errcode)
 
 
 setup(
@@ -37,11 +37,16 @@ setup(
     author_email='edwinng@uber.com',
     description=DESCRIPTION,
     include_package_data=True,
+    setup_requires=[
+        # Setuptools 18.0 properly handles Cython extensions.
+        'setuptools>=18.0',
+        'cython'
+    ],
     install_requires=requirements('requirements.txt'),
     tests_require=requirements('requirements-test.txt'),
-    # cmdclass={
-    #     'test': PyTest
-    # },
+    cmdclass={
+        'test': PyTest
+    },
     license='closed',
     long_description=read_long_description(),
     name='orbit',
