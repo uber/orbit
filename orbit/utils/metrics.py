df509cb5a92f25f476d9c622c8d5ab114d978520
Edwin Ng <edwinng@uber.com>
2020-03-23 13:23:56 -0700

    added two forecast metrics

--- orbit/utils/metrics.py
+++ orbit/utils/metrics.py
@@ -1,8 +1,8 @@
 import numpy as np
 EPS = 1e-5
 
+# utilities/loss to evaluate model performance
 
-# a few utility functions to calculate mape/wmape/smape metrics
 def smape(actual, predicted):
     ''' calculate symmetric mape
 
@@ -23,7 +23,7 @@ def smape(actual, predicted):
     return 2 * np.mean(np.abs(actual - predicted) / (np.abs(actual) + np.abs(predicted)))
 
 
-def mape(actual, pred):
+def mape(actual, predicted):
     ''' calculate mape metric
 
     Parameters
@@ -39,10 +39,10 @@ def mape(actual, pred):
         mape value
     '''
     actual = actual[np.abs(actual) > EPS]
-    return np.mean(np.abs((actual - pred) / actual))
+    return np.mean(np.abs((actual - predicted) / actual))
 
 
-def wmape(actual, pred):
+def wmape(actual, predicted):
     ''' calculate weighted mape metric
 
     Parameters
@@ -59,4 +59,44 @@ def wmape(actual, pred):
     '''
     actual = actual[np.abs(actual) > EPS]
     weights = np.abs(actual) / np.sum(np.abs(actual))
-    return np.sum(weights * np.abs((actual - pred) / actual))
+    return np.sum(weights * np.abs((actual - predicted) / actual))
+
+
+def mse(actual, predicted):
+    ''' calculate mse metric
+
+    Parameters
+    ----------
+    actual: array-like
+        true values
+    pred: array-like
+        prediction values
+
+    Returns
+    -------
+    float
+        mse value
+    '''
+
+    return np.mean(np.square(actual - predicted))
+
+
+def mse_naive(observed, exclude_leading_zeros=True):
+    ''' calculate mse using last observed value as predictor
+    Parameters
+    ----------
+    actual: array-like
+        observed values
+    Returns
+    -------
+    float
+        mse value
+    '''
+    actual = observed[1:]
+    predicted = observed[:-1]
+    if exclude_leading_zeros:
+        first_non_zero = np.min(np.nonzero(predicted))
+        actual = actual[first_non_zero:]
+        predicted = predicted[first_non_zero:]
+
+    return mse(actual, predicted)
