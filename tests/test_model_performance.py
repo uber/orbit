8da726b6246c62626642f614b2eec1cf04d99b6b
Steve Yang <steve.yang@uber.com>
2020-04-24 16:03:39 -0700

    model performance tests placeholder

---
+++ tests/test_model_performance.py
@@ -0,0 +1,15 @@
+from orbit.lgt import LGT
+
+@pytest.fixture(scope='module')
+def lgt_mcmc_mean_with_regression(synthetic_data):
+    train_df, test_df, coef = synthetic_data
+    lgt = LGT(
+        response_col='response',
+        date_col='week',
+        regressor_col=train_df.columns.tolist()[2:],
+        seasonality=52,
+        sample_method='mcmc',
+        predict_method='mean',
+    )
+    lgt.fit(train_df)
+    return lgt
