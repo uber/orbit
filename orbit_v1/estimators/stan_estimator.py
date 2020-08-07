43d80f56994689cb8e8479399487977f0884b415
Steve Yang <steve.yang@uber.com>
2020-08-07 15:41:42 -0700

    Implement MCMC Stan Estimator with UT

--- orbit_v1/estimators/stan_estimator.py
+++ orbit_v1/estimators/stan_estimator.py
@@ -1,3 +1,4 @@
+from abc import abstractmethod
 import logging
 import pandas as pd
 import multiprocessing
@@ -20,6 +21,10 @@ class StanEstimator(BaseEstimator):
         self.cores = cores
         self.seed = seed
         self.algorithm = algorithm
+
+        # stan_init fallback if not provided in model
+        # this arg is passed in through `model_payload` in `fit()` to override
+        self.stan_init = 'random'
 
         # init computed configs
         # todo: change to private variables
@@ -45,6 +50,10 @@ class StanEstimator(BaseEstimator):
                 self.chains, self.cores, self.num_warmup_per_chain, self.num_sample_per_chain)
             logging.info(msg)
 
+    @abstractmethod
+    def fit(self, **kwargs):
+        raise NotImplementedError('Concrete fit() method must be implemented')
+
 
 class StanEstimatorMCMC(StanEstimator):
     """Stan Estimator for MCMC Sampling"""
@@ -58,34 +67,49 @@ class StanEstimatorMCMC(StanEstimator):
     def _set_computed_stan_mcmc_configs(self):
         self.stan_mcmc_args = update_dict({}, self.stan_mcmc_args)
 
-    def fit(self, model_payload):
+    def fit(self, stan_model_name, model_param_names, data_input, stan_init=None):
         """Estimate model posteriors with Stan
 
         Parameters
         ----------
-        model_payload : dict
-            Model specific payload such as `stan_model_name`, `data_input`,
-            `model_param_names`, `stan_init`
+        stan_model_name : str
+            name of stan model
+        model_param_names : list
+            list of strings of model parameters names to extract
+        data_input : dict
+            key-value pairs of data input as required by definition in stan model
+        stan_init : float or np.array
+            initial sampler value. If None, 'random' is used
 
         """
-        compiled_stan_file = get_compiled_stan_model(model_payload['stan_model_name'])
+        compiled_stan_file = get_compiled_stan_model(stan_model_name)
+        # todo: to decouple estimator and model, we cannot pass predefined
+        #   stan_init from the model because that depends on number of chains
+        #   which should only be available in the estimator. test to make sure
+        #   passing callable from the model will work as seen in `initfun1()`
+        #   here: https://pystan.readthedocs.io/en/latest/api.html
+        stan_init = stan_init or self.stan_init  # if None, use default as defined in class variable
 
         stan_mcmc_fit = compiled_stan_file.sampling(
-            data=model_payload['data_input'],
-            pars=model_payload['model_param_names'],
+            data=data_input,
+            pars=model_param_names,
             iter=self.num_iter_per_chain,
             warmup=self.num_warmup_per_chain,
             chains=self.chains,
             n_jobs=self.cores,
-            init=model_payload['stan_init'],
+            # fall back to default if not provided by model payload
+            init=stan_init,
             seed=self.seed,
             algorithm=self.algorithm,
             control=self.stan_mcmc_control,
             **self.stan_mcmc_args
         )
 
+        # extract `lp__` in addition to defined model params
+        model_param_names_with_lp = model_param_names[:] + ['lp__']
+
         stan_extract = stan_mcmc_fit.extract(
-            pars=model_payload['model_param_names'],
+            pars=model_param_names_with_lp,
             permuted=False
         )
 
--- tests/test_stan_estimator.py
+++ tests/test_stan_estimator.py
@@ -1,8 +1,19 @@
-import mock
-
-from orbit.estimator import Estimator
+from orbit_v1.estimators.stan_estimator import StanEstimatorMCMC
 
 
-def test_prediction_flow():
-    # use mock `_predict_once`
-    pass
+def test_stan_estimator_mcmc(stan_estimator_lgt_model_input):
+    stan_model_name, model_param_names, data_input = stan_estimator_lgt_model_input
+
+    # create estimator
+    mcmc_estimator = StanEstimatorMCMC()
+
+    # extract posterior samples
+    stan_extract = mcmc_estimator.fit(
+        stan_model_name=stan_model_name,
+        model_param_names=model_param_names,
+        data_input=data_input
+    )
+
+    expected_extract_keys = model_param_names[:] + ['lp__']
+
+    assert set(expected_extract_keys) == set(stan_extract.keys())
