96797f19936883e47b9fba65c1f513679e40f6f5
Steve Yang <steve.yang@uber.com>
2020-08-13 14:36:01 -0700

    refactor unit test directory stucture

--- tests/orbit/test_lgt.py
+++ tests/orbit/test_lgt.py
@@ -5,9 +5,6 @@ import pytest
 from orbit.lgt import LGT
 from orbit.exceptions import IllegalArgument, EstimatorException
 
-from orbit_v1.models.lgt import BaseLGT, LGTFull, LGTAggregated, LGTMAP
-from orbit_v1.estimators.stan_estimator import StanEstimatorMCMC, StanEstimatorVI, StanEstimatorMAP
-
 
 @pytest.mark.parametrize("infer_method", ["map", "vi", "mcmc"])
 @pytest.mark.parametrize("predict_method", ["map", "mean", "median", "full"])
@@ -72,6 +69,7 @@ def test_non_seasonal_fit(m3_monthly_data, seasonality_sm_input):
     predicted_df = mod.predict(df=m3_monthly_data)
     assert len(mod.aggregated_posteriors['map']) == 10
     assert predicted_df.shape == (68, 2)
+
 
 @pytest.mark.parametrize("infer_method", ["map", "vi", "mcmc"])
 @pytest.mark.parametrize("predict_method", ["map", "mean", "median", "full"])
@@ -309,115 +307,3 @@ def test_fit_and_predict_with_regression_all_int(synthetic_data):
 
     assert predict_df.shape == expected_shape
     assert predict_df.columns.tolist() == expected_columns
-
-
-def test_base_lgt_init():
-    lgt = BaseLGT()
-
-    is_fitted = lgt.is_fitted()
-
-    stan_data_input = lgt._get_stan_data_input()
-    model_param_names = lgt._get_model_param_names()
-    stan_init = lgt._get_stan_init()
-
-    assert not is_fitted  # model is not yet fitted
-    assert not stan_data_input  # should only be initialized and not set
-    assert model_param_names  # model param names should already be set
-    # todo: change when stan_init callable is implemented
-    assert not stan_init
-
-
-@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI])
-def test_lgt_full_univariate(synthetic_data, estimator_type):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTFull(
-        response_col='response',
-        date_col='week',
-        prediction_percentiles=[5, 95],
-        seasonality=52,
-        num_warmup=50,
-        verbose=False,
-        estimator_type=estimator_type
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
-    expected_columns = ['week', 5, 'prediction', 95]
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 13
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
-
-
-@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI])
-def test_lgt_non_seasonal_fit(synthetic_data, estimator_type):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTFull(
-        response_col='response',
-        date_col='week',
-        estimator_type=estimator_type
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
-    expected_columns = ['week', 'prediction']
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 11
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
-
-
-@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI])
-def test_lgt_aggregated_univariate(synthetic_data, estimator_type):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTAggregated(
-        response_col='response',
-        date_col='week',
-        seasonality=52,
-        num_warmup=50,
-        verbose=False,
-        estimator_type=estimator_type
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
-    expected_columns = ['week', 'prediction']
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 13
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
-
-
-def test_lgt_map_univariate(synthetic_data):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTMAP(
-        response_col='response',
-        date_col='week',
-        seasonality=52,
-        num_warmup=50,
-        verbose=False,
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
-    expected_columns = ['week', 'prediction']
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 12  # no `lp__` parameter in optimizing()
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
