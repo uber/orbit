1ab879057bc2310ae1dc67c0746f7c8bb0622f7b
Steve Yang <steve.yang@uber.com>
2020-08-20 12:47:58 -0700

    update base estimator and write `fit()` abstract method with signature

--- orbit/estimators/base_estimator.py
+++ orbit/estimators/base_estimator.py
@@ -5,6 +5,10 @@ import multiprocessing
 
 
 class BaseEstimator(object):
-    """Base Estimator class for shared methods for Inference Engines"""
+    """Base Estimator class for both Stan and Pyro Estimator"""
     def __init__(self, verbose=False):
         self.verbose = verbose
+
+    @abstractmethod
+    def fit(self, model_name, model_param_names, data_input, init_values=None):
+        raise NotImplementedError('Concrete fit() method must be implemented')
