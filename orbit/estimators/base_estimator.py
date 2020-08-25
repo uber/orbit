b9b5da8d09ea2eba7b1e7e87183ebc20abe5ec3c
Steve Yang <steve.yang@uber.com>
2020-08-24 20:56:24 -0700

    Revert "set numpy random seed"
    This reverts commit a8a9d81e3cad55dd6dfa9483c883ae25e075c6ee.

--- orbit/estimators/base_estimator.py
+++ orbit/estimators/base_estimator.py
@@ -1,15 +1,13 @@
 from abc import ABCMeta, abstractmethod
 import numpy as np
+import pandas as pd
+import multiprocessing
 
 
 class BaseEstimator(object):
     """Base Estimator class for both Stan and Pyro Estimator"""
-    def __init__(self, seed=8888, verbose=False):
-        self.seed = seed
+    def __init__(self, verbose=False):
         self.verbose = verbose
-
-        # set random state
-        np.random.seed(self.seed)
 
     @abstractmethod
     def fit(self, model_name, model_param_names, data_input, init_values=None):
