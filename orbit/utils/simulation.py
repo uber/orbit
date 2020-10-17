908ec22172f860590af957481fe5029d8365ba4a
Edwin Ng <edwinng@uber.com>
2020-10-17 09:05:21 -0700

    relabel

--- orbit/utils/simulation.py
+++ orbit/utils/simulation.py
@@ -5,13 +5,13 @@ import math
 from orbit.exceptions import IllegalArgument
 
 
-def make_trend(series_len, rw_loc=0.001, rw_scale=0.1, type='rw', seed=1):
+def make_trend(series_len, rw_loc=0.0, rw_scale=0.1, method='rw', seed=1):
     """ Module to generate time-series trend with different methods
     Parameters
     ----------
     series_len: int
         total length of series
-    type: str ['arma', 'rw']
+    method: str ['arma', 'rw']
     rw_loc: float
         mean of random walk
     rw_scale: float
@@ -22,14 +22,16 @@ def make_trend(series_len, rw_loc=0.001, rw_scale=0.1, type='rw', seed=1):
       np.array-llike with length equals `series_len`
     """
     # make trend
-    if type == "rw":
+    if method == "rw":
         rw = np.random.default_rng(seed).normal(rw_loc, rw_scale, series_len)
         trend = np.cumsum(rw)
-    elif type == "arma":
+    elif method == "arma1":
         # TODO: consider parameterize this
         arparams = np.array([.25])
         maparams = np.array([.6])
+        # add zero-lag and negate
         ar = np.r_[1, -arparams]
+        # add zero-lag
         ma = np.r_[1, maparams]
         arma_process = sm.tsa.ArmaProcess(ar, ma)
         trend = arma_process.generate_sample(series_len)
@@ -39,7 +41,7 @@ def make_trend(series_len, rw_loc=0.001, rw_scale=0.1, type='rw', seed=1):
     return trend
 
 
-def make_seasonality(series_len, seasonality, order=3, duration=1, scale=.05, type='discrete', seed=1):
+def make_seasonality(series_len, seasonality, order=3, duration=1, scale=.05, method='discrete', seed=1):
     """ Module to generate time-series seasonality with different methods
     series_len: int
         total length of series
@@ -52,14 +54,14 @@ def make_seasonality(series_len, seasonality, order=3, duration=1, scale=.05, ty
         in non-fourier type of seasonality ONLY.
     scale: float
         scale parameter of seasonality generation
-    type: str ['discrete', 'fourier']
+    method: str ['discrete', 'fourier']
     seed: int
     Returns
     -------
       np.array-llike with length equals `series_len`
     """
     if seasonality > 1:
-        if type == 'fourier':
+        if method == 'fourier':
             t = np.arange(0, series_len)
             out = []
             for i in range(1, order + 1):
@@ -85,6 +87,65 @@ def make_seasonality(series_len, seasonality, order=3, duration=1, scale=.05, ty
     return seas
 
 
+def make_regression(series_len, coefs, loc=0.0, scale=0.5, cov=None,
+                    noise_scale=1.0, relevance=1.0, sparsity=0.2, seed=1):
+    """ Module to generate multiplicative time-series with trend, seasonality and regression components
+    Parameters
+    ----------
+    series_len: int
+    coefs: 1-D array_like
+        values used as regression coefficients
+    loc: float
+        location parameter to generate regressor values
+    scale: float
+        scale parameter to generate regressor values
+    cov: 2-D array_like, of shape (num_of_regressors, num_of_regressors)
+        covariance of regressors in log unit scale
+    relevance: float
+        0 to 1; smaller value indicates less number of total useful regressors
+    sparsity: float
+        0 to 1 to control probability (= 1 - sparsity) at time t of a regressor value > 0
+    seed: int
+    Notes
+    ------
+        Some ideas are from https://scikit-learn.org/stable/auto_examples/linear_model/plot_bayesian_ridge.html
+        and https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.BayesianRidge.html#sklearn.linear_model.BayesianRidge
+    """
+
+    num_of_regressors = len(coefs)
+    if (relevance > 0.0) and (relevance < 1.0):
+        num_of_irr_coefs = int(num_of_regressors * (1 - relevance))
+        coefs = np.copy(coefs)
+        irr_idx = np.random.choice(num_of_regressors, num_of_irr_coefs, replace=False)
+        coefs[irr_idx] = 0.0
+
+    if cov is None:
+        x = np.random.default_rng(seed).normal(
+            loc,
+            scale,
+            series_len * num_of_regressors
+        ).reshape(series_len, -1)
+    else:
+        x = np.random.default_rng(seed).multivariate_normal(
+            np.array([loc] * num_of_regressors, dtype=np.float64),
+            cov,
+            series_len
+        )
+    # control probability of regression kick-in
+    if (sparsity > 0.0) and (sparsity < 1.0):
+        z = np.random.default_rng(seed).binomial(
+            1,
+            1 - sparsity,
+            series_len * num_of_regressors
+        ).reshape(series_len, -1)
+        x = x * z
+
+    noise = np.random.default_rng(seed).normal(0, noise_scale, series_len)
+    # make observed response
+    y = np.matmul(x, coefs) + noise
+    return x, y, coefs
+
+
 def make_ts_multiplicative(series_len=200, seasonality=-1, coefs=None, regressor_relevance=0.0,
                            regressor_log_loc=0.0, regressor_log_scale=0.2,
                            regressor_log_cov=None,
@@ -92,7 +153,7 @@ def make_ts_multiplicative(series_len=200, seasonality=-1, coefs=None, regressor
                            obs_val_base=1000, regresspr_val_base=1000,
                            trend_type='rw', rw_loc=0.001, rw_scale=0.1, seas_scale=.05,
                            response_col='y', seed=0):
-    """
+    """ [Deprecated] Module to generate multiplicative time-series with trend, seasonality and regression components
     Parameters
     ----------
     series_len: int
@@ -143,33 +204,8 @@ def make_ts_multiplicative(series_len=200, seasonality=-1, coefs=None, regressor
             1, regression_sparsity, series_len * num_of_regressors).reshape(series_len, -1)
         x_obs = x_log1p * z
 
-    # make trend
-    # if trend_type == "rw":
-    #     rw = np.random.default_rng(seed).normal(rw_loc, rw_scale, series_len)
-    #     trend = np.cumsum(rw)
-    # elif trend_type == "arma":
-    #     # TODO: consider parameterize this
-    #     arparams = np.array([.25])
-    #     maparams = np.array([.6])
-    #     ar = np.r_[1, -arparams]
-    #     ma = np.r_[1, maparams]
-    #     arma_process = sm.tsa.ArmaProcess(ar, ma)
-    #     trend = arma_process.generate_sample(series_len)
-    # else:
-    #     raise IllegalArgument("Invalid trend_type.")
-    trend = make_trend(series_len=series_len, rw_loc=rw_loc, rw_scale=rw_scale, type=trend_type, seed=seed)
-
-    # make seasonal component
-    # if seasonality > 1:
-    #     init_seas = np.zeros(seasonality)
-    #     init_seas[:-1] = np.random.default_rng(seed).normal(0, seas_scale, seasonality - 1)
-    #     init_seas[seasonality - 1] = -1 * np.sum(init_seas)
-    #     seas = np.zeros(series_len)
-    #     for idx in range(series_len):
-    #         seas[idx] = init_seas[idx % seasonality]
-    # else:
-    #     seas = np.zeros(series_len)
-    seas = make_seasonality(seasonality=seasonality, series_len=series_len, scale=seas_scale, seed=seed)
+    trend = make_trend(series_len=series_len, rw_loc=rw_loc, rw_scale=rw_scale, method=trend_type, seed=seed)
+    seas = make_seasonality(series_len=series_len, seasonality=seasonality, scale=seas_scale, seed=seed)
 
     # make noise
     obs_log_scale = noise_to_signal_ratio * regressor_log_scale
