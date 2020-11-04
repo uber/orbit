9262f3e4e5368890ef7d96827153aeb91ff38dc9
Zhishi Wang <zhishiw@uber.com>
2020-11-04 12:27:54 -0800

    parse date column

--- orbit/utils/dataset.py
+++ orbit/utils/dataset.py
@@ -1,8 +1,7 @@
 import pandas as pd
 
 def load_iclaims():
-    """
-    Load iclaims dataset
+    """Load iclaims dataset
 
     Returns
     -------
@@ -11,15 +10,15 @@ def load_iclaims():
     Notes
     -----
     https://fred.stlouisfed.org/series/ICNSA
+    https://trends.google.com/trends/?geo=US
     """
     url = 'https://raw.githubusercontent.com/uber/orbit/master/examples/data/iclaims_example.csv'
-    df = pd.read_csv(url)
+    df = pd.read_csv(url, parse_dates=['week'])
 
     return df
 
 def load_m4weekly():
-    """
-    Load iclaims dataset
+    """Load m4 weekly dataset
 
     Returns
     -------
@@ -30,13 +29,12 @@ def load_m4weekly():
     https://forecasters.org/resources/time-series-data/m4-competition/
     """
     url = 'https://raw.githubusercontent.com/uber/orbit/master/examples/data/m4_weekly.csv'
-    df = pd.read_csv(url)
+    df = pd.read_csv(url, parse_dates=['date'])
 
     return df
 
 def load_m5daily():
-    """
-    Load iclaims dataset
+    """Load m5 daily dataset
 
     Returns
     -------
@@ -47,6 +45,6 @@ def load_m5daily():
     https://www.kaggle.com/c/m5-forecasting-accuracy
     """
     url = 'https://raw.githubusercontent.com/uber/orbit/master/examples/data/m5_agg_demand_full.csv'
-    df = pd.read_csv(url)
+    df = pd.read_csv(url, parse_dates=['date'])
 
     return df
