0b968d10d49a7ec651fa459e1ccd0fcb38d5b4e3
Edwin Ng <edwinng@uber.com>
2020-11-09 14:55:58 -0800

    adding logo and some style for tutorial and docs

--- docs/conf.py
+++ docs/conf.py
@@ -12,6 +12,7 @@
 
 import os
 import sys
+import sphinx_rtd_theme
 sys.path.insert(0, os.path.abspath('..'))
 import matplotlib
 import orbit
@@ -38,6 +39,7 @@ extensions = [
     # 'sphinx.ext.autosummary',
     # 'sphinx.ext.doctest',
     'sphinx.ext.mathjax',
+    'sphinx.ext.githubpages',
     # 'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
     'nbsphinx',
@@ -62,6 +64,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 html_theme = 'sphinx_rtd_theme'
+html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 html_context = {
   'display_github': True,
@@ -70,10 +73,23 @@ html_context = {
   'github_version': 'master/docs/',
 }
 
+# logo
+html_logo = 'img/orbit-logo-black.png'
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {
+    # 'logo_only': False,
+    'navigation_depth': 3,
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_style = 'css/orbit.css'
 
 napoleon_numpy_docstring = True
 napoleon_include_init_with_doc = True
