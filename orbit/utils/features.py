07cde693606c05db200b0e1aea71e18bb71687d5
Zhishi Wang <zhishiw@uber.com>
2020-11-12 19:08:19 -0800

    fourier seris column order correction

--- orbit/utils/features.py
+++ orbit/utils/features.py
@@ -37,6 +37,35 @@ def make_fourier_series(dates, period, order=3):
     return out
 
 
+def make_fourier_series2(n, period, order=3):
+    """ Given dates array, cyclical period and order.  Return a set of fourier series.
+    Parameters
+    ----------
+    dates: 1D array-like
+        Array of datetime stamp
+    period: int
+        Unlike version1, the prophet version, let's test on unnormalized period
+    order: int
+        Number of components for each sin() or cos() series.
+    Returns
+    -------
+    2D array-like
+        2D array where each column represents the series with a specific order fourier constructed by sin() or cos().
+    Notes
+    -----
+        1. See https://otexts.com/fpp2/complexseasonality.html
+        2. Original idea from https://github.com/facebook/prophet under
+    """
+    t = np.arange(1, n + 1)
+    out = list()
+    for i in range(1, order + 1):
+        x = 2.0 * i * np.pi * t / period
+        out.append(np.cos(x))
+        out.append(np.sin(x))
+    out = np.column_stack(out)
+    return out
+
+
 def make_fourier_series_df(df, date_col, period, order=3, prefix='', suffix=''):
     """ Given a data-frame, cyclical period and order.  Return a set of fourier series in a dataframe.
     Parameters
