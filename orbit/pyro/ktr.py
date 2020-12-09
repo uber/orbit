0f68e24eeea8d7af7c18ed45b388cfc6c74d0692
Edwin Ng <edwinng@uber.com>
2020-12-09 11:17:41 -0800

    Update ktr.py

--- orbit/pyro/ktr.py
+++ orbit/pyro/ktr.py
@@ -73,7 +73,6 @@ class Model:
             regressors = rr
 
         response -= seas_term
-        meany -= torch.mean(seas_term)
         response_tran = response - meany
 
         # sampling begins here
@@ -87,7 +86,7 @@ class Model:
 
         # levels sampling
         if len(lev_knot_loc) > 0:
-            lev_knot_tran = pyro.sample("lev_knot", dist.Laplace(lev_knot_loc - meany, lev_knot_scale).expand([n_knots_lev]))
+            lev_knot_tran = pyro.sample("lev_knot", dist.Normal(lev_knot_loc - meany, lev_knot_scale).expand([n_knots_lev]))
             lev = (lev_knot_tran @ k_lev.transpose(-2, -1))
         else:
             lev_knot_tran = pyro.sample("lev_knot", dist.Laplace(0, lev_knot_scale).expand([n_knots_lev]))
