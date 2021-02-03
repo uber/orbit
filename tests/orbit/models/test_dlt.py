04f30fb6e9452565505f183ad4ca25b7ba6a76f6
Zhishi Wang <zhishiw@uber.com>
2021-02-02 18:21:48 -0800

    unit test for fixed sm input and regression penalty

--- tests/orbit/models/test_dlt.py
+++ tests/orbit/models/test_dlt.py
@@ -351,3 +351,63 @@ def test_dlt_full_reproducibility(synthetic_data, estimator_type, regressor_sign
 
     # assert prediction is reproducible
     assert all(predict_df_first == predict_df_second)
+
+
+@pytest.mark.parametrize("regression_penalty", ['fixed_ridge', 'lasso', 'auto_ridge'])
+def test_dlt_regression_penalty(synthetic_data, regression_penalty):
+    train_df, test_df, coef = synthetic_data
+
+    dlt = DLTMAP(
+        response_col='response',
+        date_col='week',
+        regressor_col=train_df.columns.tolist()[2:],
+        regressor_penalty=regression_penalty,
+        seasonality=52,
+        num_warmup=50,
+        verbose=False,
+    )
+
+    dlt.fit(train_df)
+    predict_df = dlt.predict(test_df)
+
+    regression_out = dlt.get_regression_coefs()
+    num_regressors = regression_out.shape[0]
+
+    expected_columns = ['week', 'prediction']
+    expected_shape = (51, len(expected_columns))
+    expected_regression_shape = (6, 3)
+
+    assert predict_df.shape == expected_shape
+    assert predict_df.columns.tolist() == expected_columns
+    assert regression_out.shape == expected_regression_shape
+    assert num_regressors == len(train_df.columns.tolist()[2:])
+
+
+@pytest.mark.parametrize("slope_sm_input", [0.0, 0.5, 1.0])
+def test_dlt_fixed_sm_input(synthetic_data, slope_sm_input):
+    train_df, test_df, coef = synthetic_data
+
+    dlt = DLTMAP(
+        response_col='response',
+        date_col='week',
+        regressor_col=train_df.columns.tolist()[2:],
+        slope_sm_input=slope_sm_input,
+        seasonality=52,
+        num_warmup=50,
+        verbose=False,
+    )
+
+    dlt.fit(train_df)
+    predict_df = dlt.predict(test_df)
+
+    regression_out = dlt.get_regression_coefs()
+    num_regressors = regression_out.shape[0]
+
+    expected_columns = ['week', 'prediction']
+    expected_shape = (51, len(expected_columns))
+    expected_regression_shape = (6, 3)
+
+    assert predict_df.shape == expected_shape
+    assert predict_df.columns.tolist() == expected_columns
+    assert regression_out.shape == expected_regression_shape
+    assert num_regressors == len(train_df.columns.tolist()[2:])
