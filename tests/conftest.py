51170d5d9604d2322a445e79a1d393e4f44a5fe4
Zhishi Wang <zhishiw@uber.com>
2021-02-04 18:20:49 -0800

    ktr unit test

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import numpy as np
 import pkg_resources
 
 from .utils.utils import make_synthetic_series
+from orbit.utils.simulation import make_trend, make_seasonality, make_regression
 
 
 @pytest.fixture
@@ -43,11 +44,18 @@ def synthetic_data():
 
 
 @pytest.fixture
-def synthetic_data_daily(seed=127, date_col='day', periodicity=365, total_cycles=4, harmonics=None, freq='1D'):
-    df, coef = make_synthetic_series(seed=127)
+def make_daily_data():
+    n_obs = 365 * 3
+    seed = 2020
+    rw = make_trend(n_obs, rw_loc=0.02, rw_scale=0.1, seed=seed)
+    fs = make_seasonality(n_obs, seasonality=365, method='fourier', order=5, seed=seed)
+    coef = [0.2, 0.1, 0.3]
+    x, y, coef = make_regression(n_obs, coef, scale=2.0, seed=seed)
 
-    train_df = df[df['week'] <= '2019-01-01']
-    test_df = df[df['week'] > '2019-01-01']
+    df = pd.DataFrame(np.concatenate([(rw + fs + y).reshape(-1, 1), x], axis=1), columns= ['response'] + list('abc'))
+    df['date'] = pd.date_range(start='2016-01-01', periods=n_obs)
+    train_df = df[df['date'] < '2018-01-01']
+    test_df = df[df['date'] >= '2018-01-01']
 
     return train_df, test_df, coef
 
