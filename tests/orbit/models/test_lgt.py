d1b7f65e5090cf9a0587af4b5878bbe32b9d5229
Edwin Ng <edwinng@uber.com>
2021-02-17 15:10:57 -0800

    Provide Inference for MAP and Aggregated Estimator (#359)

--- tests/orbit/models/test_lgt.py
+++ tests/orbit/models/test_lgt.py
@@ -1,15 +1,16 @@
-import copy
+from copy import copy
 import pytest
 import numpy as np
 
 from orbit.estimators.pyro_estimator import PyroEstimator, PyroEstimatorVI, PyroEstimatorMAP
 from orbit.estimators.stan_estimator import StanEstimator, StanEstimatorMCMC, StanEstimatorVI, StanEstimatorMAP
-from orbit.models.lgt import BaseLGT, LGTFull, LGTAggregated, LGTMAP
+from orbit.models.lgt import LGTMAP, LGTFull, LGTAggregated
 from orbit.constants.constants import PredictedComponents
 
 
-def test_base_lgt_init():
-    lgt = BaseLGT()
+@pytest.mark.parametrize("model_class", [LGTMAP, LGTFull, LGTAggregated])
+def test_base_ets_init(model_class):
+    lgt = model_class()
 
     is_fitted = lgt.is_fitted()
 
@@ -17,121 +18,96 @@ def test_base_lgt_init():
     model_param_names = lgt._get_model_param_names()
     init_values = lgt._get_init_values()
 
-    assert not is_fitted  # model is not yet fitted
-    assert not model_data_input  # should only be initialized and not set
-    assert model_param_names  # model param names should already be set
-    # todo: change when init_values callable is implemented
+    # model is not yet fitted
+    assert not is_fitted
+    # should only be initialized and not set
+    assert not model_data_input
+    # model param names should already be set
+    assert model_param_names
+    # callable is not implemented yet
     assert not init_values
 
 
-@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI])
-def test_lgt_full_univariate(synthetic_data, estimator_type):
+@pytest.mark.parametrize("seasonality", [None, 52])
+@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI, PyroEstimatorVI])
+def test_lgt_full_fit(synthetic_data, seasonality, estimator_type):
     train_df, test_df, coef = synthetic_data
+    args = {
+        'response_col': 'response',
+        'date_col': 'week',
+        'prediction_percentiles': [5, 95],
+        'seasonality': seasonality,
+        'verbose': False,
+        'estimator_type': estimator_type
+    }
+    if issubclass(estimator_type, StanEstimator):
+        expected_num_parameters = 11
+        args.update({'num_warmup': 50})
+    else:
+        # no `lp__` in pyro
+        expected_num_parameters = 10
+        args.update({'num_steps': 10})
 
-    lgt = LGTFull(
-        response_col='response',
-        date_col='week',
-        prediction_percentiles=[5, 95],
-        seasonality=52,
-        num_warmup=50,
-        verbose=False,
-        estimator_type=estimator_type
-    )
+    if seasonality == 52:
+        expected_num_parameters += 2
 
+    lgt = LGTFull(**args)
     lgt.fit(train_df)
     predict_df = lgt.predict(test_df)
 
     expected_columns = ['week', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 13
 
     assert predict_df.shape == expected_shape
     assert predict_df.columns.tolist() == expected_columns
     assert len(lgt._posterior_samples) == expected_num_parameters
 
 
-def test_lgt_full_univariate_pyro(synthetic_data):
+@pytest.mark.parametrize("seasonality", [None, 52])
+@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI, PyroEstimatorVI])
+def test_lgt_aggregated_fit(synthetic_data, seasonality, estimator_type):
     train_df, test_df, coef = synthetic_data
+    args = {
+        'response_col': 'response',
+        'date_col': 'week',
+        'prediction_percentiles': [5, 95],
+        'seasonality': seasonality,
+        'verbose': False,
+        'estimator_type': estimator_type
+    }
 
-    lgt = LGTFull(
-        response_col='response',
-        date_col='week',
-        prediction_percentiles=[5, 95],
-        seasonality=52,
-        num_steps=10,
-        verbose=False,
-        estimator_type=PyroEstimatorVI
-    )
+    if issubclass(estimator_type, StanEstimator):
+        expected_num_parameters = 11
+        args.update({'num_warmup': 50})
+    else:
+        # no `lp__` in pyro
+        expected_num_parameters = 10
+        args.update({'num_steps': 10})
+
+    if seasonality == 52:
+        expected_num_parameters += 2
+
+    lgt = LGTAggregated(**args)
 
     lgt.fit(train_df)
     predict_df = lgt.predict(test_df)
-
     expected_columns = ['week', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 12  # no `lp__` in pyro
 
     assert predict_df.shape == expected_shape
     assert predict_df.columns.tolist() == expected_columns
     assert len(lgt._posterior_samples) == expected_num_parameters
 
 
-@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI])
-def test_lgt_aggregated_univariate(synthetic_data, estimator_type):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTAggregated(
-        response_col='response',
-        date_col='week',
-        seasonality=52,
-        num_warmup=50,
-        verbose=False,
-        estimator_type=estimator_type
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
-    expected_columns = ['week', 'prediction']
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 13
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
-
-
-def test_lgt_aggregated_univariate_pyro(synthetic_data):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTAggregated(
-        response_col='response',
-        date_col='week',
-        seasonality=52,
-        verbose=False,
-        num_steps=10,
-        estimator_type=PyroEstimatorVI
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
-    expected_columns = ['week', 'prediction']
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 12  # no `lp__` in pyro
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
-
-
+@pytest.mark.parametrize("seasonality", [None, 52])
 @pytest.mark.parametrize("estimator_type", [StanEstimatorMAP, PyroEstimatorMAP])
-def test_lgt_map_univariate(synthetic_data, estimator_type):
+def test_lgt_map_fit(synthetic_data, seasonality, estimator_type):
     train_df, test_df, coef = synthetic_data
 
     lgt = LGTMAP(
         response_col='response',
         date_col='week',
-        seasonality=52,
+        seasonality=seasonality,
         verbose=False,
         estimator_type=estimator_type
     )
@@ -139,55 +115,12 @@ def test_lgt_map_univariate(synthetic_data, estimator_type):
     lgt.fit(train_df)
     predict_df = lgt.predict(test_df)
 
-    expected_columns = ['week', 'prediction']
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 12  # no `lp__` parameter in optimizing()
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
-
-
-@pytest.mark.parametrize("estimator_type", [StanEstimatorMCMC, StanEstimatorVI])
-def test_lgt_non_seasonal_fit(synthetic_data, estimator_type):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTFull(
-        response_col='response',
-        date_col='week',
-        estimator_type=estimator_type,
-        num_warmup=50,
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
+    expected_num_parameters = 10
     expected_columns = ['week', 'prediction_5', 'prediction', 'prediction_95']
+    if seasonality == 52:
+        expected_num_parameters += 2
+
     expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 11
-
-    assert predict_df.shape == expected_shape
-    assert predict_df.columns.tolist() == expected_columns
-    assert len(lgt._posterior_samples) == expected_num_parameters
-
-
-def test_lgt_non_seasonal_fit_pyro(synthetic_data):
-    train_df, test_df, coef = synthetic_data
-
-    lgt = LGTFull(
-        response_col='response',
-        date_col='week',
-        estimator_type=PyroEstimatorVI,
-        num_steps=10
-    )
-
-    lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
-
-    expected_columns = ['week', 'prediction_5', 'prediction', 'prediction_95']
-    expected_shape = (51, len(expected_columns))
-    expected_num_parameters = 10  # no `lp__` in pyro
-
     assert predict_df.shape == expected_shape
     assert predict_df.columns.tolist() == expected_columns
     assert len(lgt._posterior_samples) == expected_num_parameters
@@ -231,6 +164,8 @@ def test_lgt_full_with_regression(synthetic_data, estimator_type, regressor_sign
             verbose=False,
             estimator_type=estimator_type
         )
+    else:
+        return None
 
     lgt.fit(train_df)
     predict_df = lgt.predict(test_df)
@@ -284,6 +219,8 @@ def test_lgt_aggregated_with_regression(synthetic_data, estimator_type, regresso
             verbose=False,
             estimator_type=estimator_type
         )
+    else:
+        return None
 
     lgt.fit(train_df)
     predict_df = lgt.predict(test_df)
@@ -291,7 +228,7 @@ def test_lgt_aggregated_with_regression(synthetic_data, estimator_type, regresso
     regression_out = lgt.get_regression_coefs()
     num_regressors = regression_out.shape[0]
 
-    expected_columns = ['week', 'prediction']
+    expected_columns = ['week', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (51, len(expected_columns))
     expected_regression_shape = (6, 3)
 
@@ -305,10 +242,7 @@ def test_lgt_aggregated_with_regression(synthetic_data, estimator_type, regresso
 
 
 @pytest.mark.parametrize(
-    "regressor_signs",
-    [
-        ['=', '=', '+'],
-    ],
+    "regressor_signs", [['=', '=', '+']],
     ids=['positive_mixed']
 )
 def test_lgt_mixed_signs_and_order(iclaims_training_data, regressor_signs):
@@ -380,14 +314,14 @@ def test_prediction_percentiles(iclaims_training_data, prediction_percentiles):
     assert predicted_df.shape[0] == df.shape[0]
 
     predicted_df = lgt.predict(df, decompose=True)
-    plot_components = [
+    predicted_components = [
         'prediction',
         PredictedComponents.TREND.value,
         PredictedComponents.SEASONALITY.value,
         PredictedComponents.REGRESSION.value]
 
     expected_columns = ['week']
-    for pc in plot_components:
+    for pc in predicted_components:
         for p in p_labels:
             expected_columns.append(pc + p)
     assert predicted_df.columns.tolist() == expected_columns
@@ -422,7 +356,7 @@ def test_lgt_full_reproducibility(synthetic_data, estimator_type, regressor_sign
 
     # first fit and predict
     lgt_first.fit(train_df)
-    posteriors_first = copy.copy(lgt_first._posterior_samples)
+    posteriors_first = copy(lgt_first._posterior_samples)
     predict_df_first = lgt_first.predict(test_df)
     regression_out_first = lgt_first.get_regression_coefs()
 
@@ -442,11 +376,9 @@ def test_lgt_full_reproducibility(synthetic_data, estimator_type, regressor_sign
     )
 
     lgt_second.fit(train_df)
-    posteriors_second = copy.copy(lgt_second._posterior_samples)
+    posteriors_second = copy(lgt_second._posterior_samples)
     predict_df_second = lgt_second.predict(test_df)
     regression_out_second = lgt_second.get_regression_coefs()
-
-    posterior_keys = posteriors_first.keys()
 
     # assert same posterior keys
     assert set(posteriors_first.keys()) == set(posteriors_second.keys())
@@ -458,10 +390,51 @@ def test_lgt_full_reproducibility(synthetic_data, estimator_type, regressor_sign
     # assert identical regression columns
     # this is also checked in posterior samples, but an extra layer just in case
     # since this one very commonly retrieved by end users
-    assert all(regression_out_first == regression_out_second)
+    assert regression_out_first.equals(regression_out_second)
 
     # assert prediction is reproducible
-    assert all(predict_df_first == predict_df_second)
+    assert predict_df_first.equals(predict_df_second)
+
+
+@pytest.mark.parametrize("seasonality", [1, 52])
+def test_lgt_map_reproducibility(synthetic_data, seasonality):
+    train_df, test_df, coef = synthetic_data
+
+    lgt1 = LGTMAP(
+        response_col='response',
+        date_col='week',
+        prediction_percentiles=[5, 95],
+        seasonality=seasonality,
+    )
+
+    # first fit and predict
+    lgt1.fit(train_df)
+    posteriors1 = copy(lgt1._aggregate_posteriors['map'])
+    prediction1 = lgt1.predict(test_df)
+
+    # second fit and predict
+    # note a new instance must be created to reset the seed
+    # note both fit and predict contain random generation processes
+    lgt2 = LGTMAP(
+        response_col='response',
+        date_col='week',
+        prediction_percentiles=[5, 95],
+        seasonality=seasonality,
+    )
+
+    lgt2.fit(train_df)
+    posteriors2 = copy(lgt2._aggregate_posteriors['map'])
+    prediction2 = lgt2.predict(test_df)
+
+    # assert same posterior keys
+    assert set(posteriors1.keys()) == set(posteriors2.keys())
+
+    # assert posterior draws are reproducible
+    for k, v in posteriors1.items():
+        assert np.allclose(posteriors1[k], posteriors2[k])
+
+    # assert prediction is reproducible
+    assert np.allclose(prediction1['prediction'].values, prediction2['prediction'].values)
 
 
 @pytest.mark.parametrize("level_sm_input", [0.0001, 0.5, 1.0])
@@ -478,17 +451,16 @@ def test_lgt_fixed_sm_input(synthetic_data, level_sm_input, seasonality_sm_input
         seasonality_sm_input=seasonality_sm_input,
         slope_sm_input=slope_sm_input,
         seasonality=52,
-        num_warmup=50,
         verbose=False,
     )
 
     lgt.fit(train_df)
-    predict_df = lgt.predict(test_df)
+    predict_df = lgt.predict(test_df, n_bootstrap_draw=100)
 
     regression_out = lgt.get_regression_coefs()
     num_regressors = regression_out.shape[0]
 
-    expected_columns = ['week', 'prediction']
+    expected_columns = ['week', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (51, len(expected_columns))
     expected_regression_shape = (6, 3)
 
@@ -496,3 +468,4 @@ def test_lgt_fixed_sm_input(synthetic_data, level_sm_input, seasonality_sm_input
     assert predict_df.columns.tolist() == expected_columns
     assert regression_out.shape == expected_regression_shape
     assert num_regressors == len(train_df.columns.tolist()[2:])
+
