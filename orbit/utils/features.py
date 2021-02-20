d8b65be112c3ef8d2d63e4979e262774e1965f01
Zhishi Wang <zhishiw@uber.com>
2021-02-19 18:37:21 -0800

    allow shift in fourier series

--- orbit/utils/features.py
+++ orbit/utils/features.py
@@ -4,16 +4,18 @@ from datetime import datetime
 from orbit.exceptions import IllegalArgument
 
 
-def make_fourier_series(dates, period, order=3):
+def make_fourier_series(n, period, order=3,  shift=0):
     """ Given dates array, cyclical period and order.  Return a set of fourier series.
     Parameters
     ----------
     dates: 1D array-like
         Array of datetime stamp
     period: int
-        Number of days of the period.
+        Unlike version1, the prophet version, let's test on unnormalized period
     order: int
         Number of components for each sin() or cos() series.
+    shift: int
+        shift of time step/index to generate the series
     Returns
     -------
     2D array-like
@@ -23,11 +25,7 @@ def make_fourier_series(dates, period, order=3):
         1. See https://otexts.com/fpp2/complexseasonality.html
         2. Original idea from https://github.com/facebook/prophet under
     """
-    t = np.array(
-        (dates - datetime(1950, 1, 1))
-            .dt.total_seconds()
-            .astype(np.float)
-    ) / (3600 * 24.)
+    t = np.arange(1, n + 1) + shift
     out = list()
     for i in range(1, order + 1):
         x = 2.0 * i * np.pi * t / period
@@ -37,7 +35,7 @@ def make_fourier_series(dates, period, order=3):
     return out
 
 
-def make_fourier_series_df(df, date_col, period, order=3, prefix='', suffix=''):
+def make_fourier_series_df(df, period, order=3, prefix='', suffix='', shift=0):
     """ Given a data-frame, cyclical period and order.  Return a set of fourier series in a dataframe.
     Parameters
     ----------
@@ -53,6 +51,8 @@ def make_fourier_series_df(df, date_col, period, order=3, prefix='', suffix=''):
         prefix of output columns label
     suffix: str
         suffix of output columns label
+    shift: int
+        shift of time step/index to generate the series
     Returns
     -------
     out: pd.DataFrame
@@ -63,7 +63,7 @@ def make_fourier_series_df(df, date_col, period, order=3, prefix='', suffix=''):
     -----
         This is calling :func:`make_fourier_series`
     """
-    fs = make_fourier_series(df[date_col], period, order=order)
+    fs = make_fourier_series(df.shape[0], period, order=order, shift=shift)
     fs_cols = []
     for i in range(1, order + 1):
         fs_cols.append('{}fs_cos{}{}'.format(prefix, i, suffix))
@@ -109,5 +109,3 @@ def make_seasonal_dummies(df, date_col, freq, sparse=True, drop_first=True):
     cols = dummies.columns.tolist()
     out = pd.concat([df, dummies], axis=1)
     return out, cols
-
-
