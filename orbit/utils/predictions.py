dee724411bea5d9a53165849dbc23e837b8c5a54
Edwin Ng <edwinng@uber.com>
2021-03-29 19:52:07 -0700

    relabel and add assert statment (#400)
    * relabel and add assert statment
    
    * Update predictions.py
    
    * Update predictions.py
    
    * Update predictions.py

--- orbit/utils/predictions.py
+++ orbit/utils/predictions.py
@@ -6,14 +6,15 @@ def prepend_date_column(predicted_df, input_df, date_col):
     """Prepends date column from `input_df` to `predicted_df`
     Parameters
     ----------
-    predicted_df: pd.DataFrame
-    input_df: pd.DataFrame
-    date_col: str
+    predicted_df : pd.DataFrame
+    input_df : pd.DataFrame
+    date_col : str
     Returns
     -------
     pd.DataFrame
     """
 
+    assert predicted_df.shape[0] == input_df.shape[0]
     other_cols = list(predicted_df.columns)
 
     # add date column
@@ -26,29 +27,42 @@ def prepend_date_column(predicted_df, input_df, date_col):
     return predicted_df
 
 
-def aggregate_predictions(predictions_dict, percentiles):
-    """Aggregates the mcmc prediction to a point estimate
+def compute_percentiles(arrays_dict, percentiles):
+    """Compute percentiles of dictionary of arrays.  Return results as dataframe.
     Parameters
     ----------
-    predictions_dict: dict
-        a dictionary where keys will be the output columns of a dataframe and
-        values are a 2d numpy array of shape (`num_samples`, prediction df length)
-    percentiles: list
-        A sorted list of one or three percentile(s) which will be used to aggregate lower, mid and upper values
+    arrays_dict : dict
+        a dictionary where each key will be the output column label of a dataframe and
+        value are a 2d array-like of shape (number of samples, prediction length) required to compute
+        percentile(s)
+    percentiles : list
+        A sorted list of percentile(s) which will be used to specify percentiles needed to compute across various
+        arrays
     Returns
     -------
     pd.DataFrame
-        The aggregated across mcmc samples with columns for `50` aka median
-        and all other percentiles specified in `percentiles`.
+        The percentiles across samples with columns for `50` aka median and all other percentiles
+        specified in `percentiles`.
     """
 
-    aggregated_dict = {}
-
-    for k, v in predictions_dict.items():
-        aggregated_array = np.percentile(v, percentiles, axis=0)
+    computed_dict = {}
+    run_check = False
+    prev_shape = None
+    for k, v in arrays_dict.items():
+        curr_shape = v.shape
+        if len(curr_shape) != 2:
+            raise ValueError("Input arrays_dict requires 2 dimensions: (number of samples, prediction length)."
+                             "Please revise input.")
+        if run_check:
+            if curr_shape[1] != prev_shape[1]:
+                raise ValueError("Input arrays has different lengths in second dimension. Please revise input.")
+        # run prediction consistency after first iteration
+        run_check = True
+        prev_shape = curr_shape
+        computed_array = np.percentile(v, percentiles, axis=0)
         columns = [k + "_" + str(p) if p != 50 else k for p in percentiles]
-        aggregated_dict[k] = pd.DataFrame(aggregated_array.T, columns=columns)
+        computed_dict[k] = pd.DataFrame(computed_array.T, columns=columns)
 
-    aggregated_df = pd.concat(aggregated_dict, axis=1)
-    aggregated_df.columns = aggregated_df.columns.droplevel()
-    return aggregated_df
+    output_df = pd.concat(computed_dict, axis=1)
+    output_df.columns = output_df.columns.droplevel()
+    return output_df
