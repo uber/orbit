46c6689d3e38f8bfcf9d4ca6a459d93b4ad70aab
Zhishi Wang <wzhishi@gmail.com>
2021-04-05 14:20:17 -0700

    unit test ktrx

--- tests/orbit/models/test_ktrx.py
+++ tests/orbit/models/test_ktrx.py
@@ -5,7 +5,7 @@ import pandas as pd
 from orbit.estimators.pyro_estimator import PyroEstimator, PyroEstimatorVI, PyroEstimatorMAP
 from orbit.estimators.stan_estimator import StanEstimator, StanEstimatorMCMC, StanEstimatorVI, StanEstimatorMAP
 from orbit.models.ktrlite import KTRLiteMAP
-from orbit.models.ktrx import BaseKTRX, KTRXFull, KTRXAggregated, KTRXMAP
+from orbit.models.ktrx import BaseKTRX, KTRXFull, KTRXAggregated
 from orbit.diagnostics.metrics import smape
 
 
@@ -27,7 +27,6 @@ def test_ktrx_pyro(make_daily_data, regressor_col):
     seasonal_knots_input = {
         '_seas_coef_knot_dates': ktrlite._coef_knot_dates,
         '_sea_coef_knot': ktrlite._aggregate_posteriors['map']['coef_knot'],
-        '_sea_rho': ktrlite.rho_coefficients,
         '_seasonality': ktrlite._seasonality,
         '_seasonality_fs_order': ktrlite._seasonality_fs_order,
     }
@@ -44,13 +43,11 @@ def test_ktrx_pyro(make_daily_data, regressor_col):
     ktrx.fit(train_df)
     predict_df = ktrx.predict(test_df)
 
-    expected_columns = ['date', 'prediction']
+    expected_columns = ['date', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (364, len(expected_columns))
-    # expected_num_parameters = 7
 
     assert predict_df.shape == expected_shape
     assert predict_df.columns.tolist() == expected_columns
-    # assert len(ktrx._posterior_samples) == expected_num_parameters
     assert smape(test_df['response'].values, predict_df['prediction'].values) <= 0.5
 
 
@@ -72,7 +69,6 @@ def test_ktrx_coef_knot_dates(make_daily_data, coefficients_knot_dates):
     seasonal_knots_input = {
         '_seas_coef_knot_dates': ktrlite._coef_knot_dates,
         '_sea_coef_knot': ktrlite._aggregate_posteriors['map']['coef_knot'],
-        '_sea_rho': ktrlite.rho_coefficients,
         '_seasonality': ktrlite._seasonality,
         '_seasonality_fs_order': ktrlite._seasonality_fs_order,
     }
@@ -90,7 +86,7 @@ def test_ktrx_coef_knot_dates(make_daily_data, coefficients_knot_dates):
     ktrx.fit(train_df)
     predict_df = ktrx.predict(test_df)
 
-    expected_columns = ['date', 'prediction']
+    expected_columns = ['date', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (364, len(expected_columns))
     expected_num_parameters = 7
 
@@ -118,7 +114,6 @@ def test_ktrx_coef_knot_distance(make_daily_data, coefficients_knot_length):
     seasonal_knots_input = {
         '_seas_coef_knot_dates': ktrlite._coef_knot_dates,
         '_sea_coef_knot': ktrlite._aggregate_posteriors['map']['coef_knot'],
-        '_sea_rho': ktrlite.rho_coefficients,
         '_seasonality': ktrlite._seasonality,
         '_seasonality_fs_order': ktrlite._seasonality_fs_order,
     }
@@ -136,7 +131,7 @@ def test_ktrx_coef_knot_distance(make_daily_data, coefficients_knot_length):
     ktrx.fit(train_df)
     predict_df = ktrx.predict(test_df)
 
-    expected_columns = ['date', 'prediction']
+    expected_columns = ['date', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (364, len(expected_columns))
     expected_num_parameters = 7
 
@@ -172,7 +167,6 @@ def test_ktrx_regressor_sign(make_daily_data, regressor_signs):
     seasonal_knots_input = {
         '_seas_coef_knot_dates': ktrlite._coef_knot_dates,
         '_sea_coef_knot': ktrlite._aggregate_posteriors['map']['coef_knot'],
-        '_sea_rho': ktrlite.rho_coefficients,
         '_seasonality': ktrlite._seasonality,
         '_seasonality_fs_order': ktrlite._seasonality_fs_order,
     }
@@ -190,7 +184,7 @@ def test_ktrx_regressor_sign(make_daily_data, regressor_signs):
     ktrx.fit(train_df)
     predict_df = ktrx.predict(test_df)
 
-    expected_columns = ['date', 'prediction']
+    expected_columns = ['date', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (364, len(expected_columns))
     expected_num_parameters = 7
 
@@ -233,7 +227,6 @@ def test_ktrx_prior_ingestion(make_daily_data, coef_prior_list):
     seasonal_knots_input = {
         '_seas_coef_knot_dates': ktrlite._coef_knot_dates,
         '_sea_coef_knot': ktrlite._aggregate_posteriors['map']['coef_knot'],
-        '_sea_rho': ktrlite.rho_coefficients,
         '_seasonality': ktrlite._seasonality,
         '_seasonality_fs_order': ktrlite._seasonality_fs_order,
     }
@@ -251,7 +244,7 @@ def test_ktrx_prior_ingestion(make_daily_data, coef_prior_list):
     ktrx.fit(train_df)
     predict_df = ktrx.predict(test_df)
 
-    expected_columns = ['date', 'prediction']
+    expected_columns = ['date', 'prediction_5', 'prediction', 'prediction_95']
     expected_shape = (364, len(expected_columns))
     expected_num_parameters = 7
 
