dac490f30434e5ef0f6aa7281c09d949c4dd7967
Edwin Ng <edwinng@uber.com>
2021-05-24 17:30:49 -0700

    fix unit test

--- tests/orbit/estimators/test_pyro_estimator.py
+++ tests/orbit/estimators/test_pyro_estimator.py
@@ -5,33 +5,29 @@ def test_pyro_estimator_vi(stan_estimator_lgt_model_input):
     stan_model_name, model_param_names, data_input = stan_estimator_lgt_model_input
 
     # create estimator
-    vi_estimator = PyroEstimatorVI()
+    vi_estimator = PyroEstimatorVI(num_steps=50)
 
     # extract posterior samples
-    extract = vi_estimator.fit(
+    posteriors, training_metrics = vi_estimator.fit(
         model_name=stan_model_name,
         model_param_names=model_param_names,
-        data_input=data_input
+        data_input=data_input,
     )
 
-    expected_extract_keys = model_param_names[:]
-
-    assert set(expected_extract_keys) == set(extract.keys())
+    assert set(model_param_names) == set(posteriors.keys())
 
 
 def test_pyro_estimator_map(stan_estimator_lgt_model_input):
     stan_model_name, model_param_names, data_input = stan_estimator_lgt_model_input
 
     # create estimator
-    map_estimator = PyroEstimatorMAP()
+    map_estimator = PyroEstimatorMAP(num_steps=50)
 
     # extract posterior samples
-    extract = map_estimator.fit(
+    posteriors, training_metrics = map_estimator.fit(
         model_name=stan_model_name,
         model_param_names=model_param_names,
-        data_input=data_input
+        data_input=data_input,
     )
 
-    expected_extract_keys = model_param_names[:]
-
-    assert set(expected_extract_keys) == set(extract.keys())
+    assert set(model_param_names) == set(posteriors.keys())
--- tests/orbit/estimators/test_stan_estimator.py
+++ tests/orbit/estimators/test_stan_estimator.py
@@ -5,15 +5,13 @@ def test_stan_estimator_mcmc(stan_estimator_lgt_model_input):
     stan_model_name, model_param_names, data_input = stan_estimator_lgt_model_input
 
     # create estimator
-    mcmc_estimator = StanEstimatorMCMC()
+    mcmc_estimator = StanEstimatorMCMC(num_warmup=50)
 
     # extract posterior samples
-    stan_extract = mcmc_estimator.fit(model_name=stan_model_name, model_param_names=model_param_names,
-                                      data_input=data_input)
+    posteriors, training_metrics = mcmc_estimator.fit(
+        model_name=stan_model_name, model_param_names=model_param_names, data_input=data_input)
 
-    expected_extract_keys = model_param_names[:] + ['lp__']
-
-    assert set(expected_extract_keys) == set(stan_extract.keys())
+    assert set(model_param_names) == set(posteriors.keys())
 
 
 def test_stan_estimator_vi(stan_estimator_lgt_model_input):
@@ -23,12 +21,10 @@ def test_stan_estimator_vi(stan_estimator_lgt_model_input):
     vi_estimator = StanEstimatorVI()
 
     # extract posterior samples
-    stan_extract = vi_estimator.fit(model_name=stan_model_name, model_param_names=model_param_names,
-                                    data_input=data_input)
+    posteriors, training_metrics = vi_estimator.fit(
+        model_name=stan_model_name, model_param_names=model_param_names, data_input=data_input)
 
-    expected_extract_keys = model_param_names[:] + ['lp__']
-
-    assert set(expected_extract_keys) == set(stan_extract.keys())
+    assert set(model_param_names) == set(posteriors.keys())
 
 
 def test_stan_estimator_map(stan_estimator_lgt_model_input):
@@ -38,9 +34,7 @@ def test_stan_estimator_map(stan_estimator_lgt_model_input):
     map_estimator = StanEstimatorMAP()
 
     # extract posterior samples
-    stan_extract = map_estimator.fit(model_name=stan_model_name, model_param_names=model_param_names,
-                                     data_input=data_input)
+    posteriors, training_metrics = map_estimator.fit(
+        model_name=stan_model_name, model_param_names=model_param_names, data_input=data_input)
 
-    expected_extract_keys = model_param_names[:]  # note no `lp__` for optimizing() method
-
-    assert set(expected_extract_keys) == set(stan_extract.keys())
+    assert set(model_param_names) == set(posteriors.keys())
