40b5f3eb15eb124befb84490bbe237ce9503c104
Edwin Ng <edwinnglabs@gmail.com>
2021-08-03 20:47:54 -0700

    fix unit test for estimator

--- tests/orbit/estimators/test_pyro_estimator.py
+++ tests/orbit/estimators/test_pyro_estimator.py
@@ -16,18 +16,3 @@ def test_pyro_estimator_vi(stan_estimator_lgt_model_input):
 
     assert set(model_param_names) == set(posteriors.keys())
 
-
-def test_pyro_estimator_map(stan_estimator_lgt_model_input):
-    stan_model_name, model_param_names, data_input = stan_estimator_lgt_model_input
-
-    # create estimator
-    map_estimator = PyroEstimatorMAP(num_steps=50)
-
-    # extract posterior samples
-    posteriors, training_metrics = map_estimator.fit(
-        model_name=stan_model_name,
-        model_param_names=model_param_names,
-        data_input=data_input,
-    )
-
-    assert set(model_param_names) == set(posteriors.keys())
