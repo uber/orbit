84e43c35760a5cdf4ee2c4a95a38b6e614e14ba4
Edwin Ng <edwinnglabs@gmail.com>
2021-08-03 20:47:54 -0700

    fix unit test for estimator

--- tests/orbit/estimators/test_stan_estimator.py
+++ tests/orbit/estimators/test_stan_estimator.py
@@ -14,19 +14,6 @@ def test_stan_estimator_mcmc(stan_estimator_lgt_model_input):
     assert set(model_param_names) == set(posteriors.keys())
 
 
-def test_stan_estimator_vi(stan_estimator_lgt_model_input):
-    stan_model_name, model_param_names, data_input = stan_estimator_lgt_model_input
-
-    # create estimator
-    vi_estimator = StanEstimatorVI()
-
-    # extract posterior samples
-    posteriors, training_metrics = vi_estimator.fit(
-        model_name=stan_model_name, model_param_names=model_param_names, data_input=data_input)
-
-    assert set(model_param_names) == set(posteriors.keys())
-
-
 def test_stan_estimator_map(stan_estimator_lgt_model_input):
     stan_model_name, model_param_names, data_input = stan_estimator_lgt_model_input
 
