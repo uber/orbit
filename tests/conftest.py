38913f5c0aa7e2d07be7f8e3e2653c5df2e0e5e4
Edwin Ng <edwinnglabs@gmail.com>
2021-09-13 15:15:00 -0700

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -33,6 +33,23 @@ def m3_monthly_data():
 
 
 @pytest.fixture
+def make_daily_data_no_coef():
+    n_obs = 365 * 3
+    seed = 2020
+    rw = make_trend(n_obs, rw_loc=0.02, rw_scale=0.1, seed=seed)
+    fs = make_seasonality(n_obs, seasonality=365, method='fourier', order=5, seed=seed)
+
+    df = pd.DataFrame(
+        np.concatenate([(rw + fs).reshape(-1, 1)], axis=1),
+        columns=['response'] + list('abc')
+    )
+    df['date'] = pd.date_range(start='2016-01-01', periods=n_obs)
+    train_df = df[df['date'] < '2018-01-01']
+    test_df = df[df['date'] >= '2018-01-01']
+
+    return train_df, test_df
+
+@pytest.fixture
 def make_daily_data():
     n_obs = 365 * 3
     seed = 2020
@@ -45,6 +62,25 @@ def make_daily_data():
     df['date'] = pd.date_range(start='2016-01-01', periods=n_obs)
     train_df = df[df['date'] < '2018-01-01']
     test_df = df[df['date'] >= '2018-01-01']
+
+    return train_df, test_df, coef
+
+@pytest.fixture
+def make_weekly_data():
+    n_obs = 52 * 4
+    seed = 2020
+    rw = make_trend(n_obs, rw_loc=1, rw_scale=0.1, seed=seed)
+    fs = make_seasonality(n_obs, seasonality=52, method='fourier', order=2, seed=seed)
+    coef = [0.2, 0.1, 0.3, 0.15, -0.2, -0.1]
+    x, y, coef = make_regression(n_obs, coef, scale=2.0, seed=seed)
+
+    df = pd.DataFrame(
+        np.concatenate([(rw + fs + y).reshape(-1, 1), x], axis=1), columns=['response'] + list('abcdef')
+    )
+    df['week'] = pd.date_range(start='2016-01-04', periods=n_obs, freq='7D')
+    df = df[['week', 'response'] + list('abcdef')]
+    train_df = df[df['week'] <= '2019-01-01'].reset_index(drop=True)
+    test_df = df[df['week'] > '2019-01-01'].reset_index(drop=True)
 
     return train_df, test_df, coef
 
@@ -135,22 +171,3 @@ def stan_estimator_lgt_model_input():
     }
     return stan_model_name, model_param_names, data_input
 
-
-@pytest.fixture
-def make_weekly_data():
-    n_obs = 52 * 4
-    seed = 2020
-    rw = make_trend(n_obs, rw_loc=1, rw_scale=0.1, seed=seed)
-    fs = make_seasonality(n_obs, seasonality=52, method='fourier', order=2, seed=seed)
-    coef = [0.2, 0.1, 0.3, 0.15, -0.2, -0.1]
-    x, y, coef = make_regression(n_obs, coef, scale=2.0, seed=seed)
-
-    df = pd.DataFrame(
-        np.concatenate([(rw + fs + y).reshape(-1, 1), x], axis=1), columns=['response'] + list('abcdef')
-    )
-    df['week'] = pd.date_range(start='2016-01-04', periods=n_obs, freq='7D')
-    df = df[['week', 'response'] + list('abcdef')]
-    train_df = df[df['week'] <= '2019-01-01'].reset_index(drop=True)
-    test_df = df[df['week'] > '2019-01-01'].reset_index(drop=True)
-
-    return train_df, test_df, coef
