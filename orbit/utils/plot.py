bc6a8bc9c47d5ab5573977c8076fec3602a5ccb2
Edwin Ng <edwinnglabs@gmail.com>
2022-01-16 16:06:50 -0800

    Update plot.py

--- orbit/utils/plot.py
+++ orbit/utils/plot.py
@@ -1,5 +1,6 @@
 import pkg_resources
 import functools
+import logging
 from matplotlib import pyplot as plt
 
 STYLE_FILE_NAME = 'plot_style.mplstyle'
@@ -16,18 +17,21 @@ def get_orbit_style():
 def orbit_style_decorator(func):
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
+        # check if arg is specified; if not, set True as default
         if "use_orbit_style" in kwargs.keys():
             use_orbit_style = kwargs['use_orbit_style']
             del kwargs['use_orbit_style']
         else:
-            # default to be True if arg is not specified
             use_orbit_style = True
+
+        # use orbit style plot if it is set to be True
         if use_orbit_style:
+            orbit_style_path = get_orbit_style()
             try:
-                orbit_style = get_orbit_style()
-                with plt.style.context(orbit_style):
+                with plt.style.context(orbit_style_path):
                     return func(*args, **kwargs)
             except:
+                logging.info('Cannot find path:{}. Use default plot style.'.format(orbit_style_path))
                 return func(*args, **kwargs)
         else:
             return func(*args, **kwargs)
