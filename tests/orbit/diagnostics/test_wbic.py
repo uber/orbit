c36ea8b4c2c172629b267b9727085298e7a3eca6
Zhishi Wang <zhishiw@uber.com>
2022-03-15 17:00:25 -0700

    test for wbic/bic

---
+++ tests/orbit/diagnostics/test_wbic.py
@@ -0,0 +1,101 @@
+import pytest
+import numpy as np
+
+from orbit.models import LGT, DLT, KTR, KTRLite
+
+@pytest.mark.parametrize("estimator", ['stan-mcmc', 'stan-map'])
+def test_wbic_dlt(make_weekly_data, estimator):
+    train_df, test_df, coef = make_weekly_data
+
+    dlt = DLT(
+        response_col='response',
+        date_col='week',
+        regressor_col = list('abcdef'),
+        seasonality=52,
+        num_warmup=100,
+        num_sample=100,
+        seed=6666,
+        estimator=estimator,
+    )
+
+    if estimator == 'stan-mcmc':
+        wbic_val = dlt.fit_wbic(df=train_df)
+        assert np.isclose(wbic_val, dlt.get_wbic_value())
+
+    elif estimator == 'stan-map':
+        dlt.fit(df=train_df)
+        bic_val = dlt.get_bic_value()
+
+
+def test_wbic_lgt(make_weekly_data):
+    train_df, test_df, coef = make_weekly_data
+
+    lgt1 = LGT(
+        response_col='response',
+        date_col='week',
+        regressor_col = list('abcdef'),
+        seasonality=52,
+        num_warmup=100,
+        num_sample=100,
+        seed=6666,
+        estimator='stan-mcmc',
+    )
+    wbic_val1 = lgt1.fit_wbic(df=train_df)
+
+    lgt2 = LGT(
+        response_col='response',
+        date_col='week',
+        regressor_col = list('abcdef'),
+        seasonality=52,
+        num_steps=100,
+        num_sample=100,
+        seed=6666,
+        estimator='pyro-svi',
+    )
+    wbic_val2 = lgt2.fit_wbic(df=train_df)
+
+    assert np.abs(wbic_val2 - wbic_val1) / wbic_val1 <= .05
+
+    lgt3 = LGT(
+        response_col='response',
+        date_col='week',
+        regressor_col = list('abcdef'),
+        seasonality=52,
+        seed=6666,
+        estimator='stan-map',
+    )
+    lgt3.fit(df=train_df)
+    bic_val = lgt3.get_bic_value()
+
+
+def test_wbic_ktr(make_weekly_data):
+    train_df, test_df, coef = make_weekly_data
+
+    ktr = KTR(
+        response_col='response',
+        date_col='week',
+        regressor_col = list('abcdef'),
+        seasonality=52,
+        num_steps=100,
+        num_sample=100,
+        seed=6666,
+        estimator='pyro-svi',
+    )
+
+    wbic_val = ktr.fit_wbic(df=train_df)
+    assert np.isclose(wbic_val, ktr.get_wbic_value())
+
+
+def test_wbic_ktrlite(make_weekly_data):
+    train_df, test_df, coef = make_weekly_data
+
+    ktrlite = KTRLite(
+        response_col='response',
+        date_col='week',
+        seasonality=52,
+        seed=6666,
+        estimator='stan-map',
+    )
+
+    ktrlite.fit(df=train_df)
+    bic_val = ktrlite.get_bic_value()
